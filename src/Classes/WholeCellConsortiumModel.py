a63db52e0c6ebfcae562f0b328410aa3f44b9430
Vincent <xuvincent1002@gmail.com>
2024-09-25 10:16:02 -0500

    working on MPNG

--- src/Classes/WholeCellConsortiumModel.py
+++ src/Classes/WholeCellConsortiumModel.py
@@ -4,16 +4,26 @@ import MicrobialConsortiumKineticModel
 class WholeCellConsortiumModel:
 
     def __init__(self):
-        self.test = 2
+        # self.MPNG = MetabolicPathwayNetworkGraph()
+        return
 
     def __getattribute__test(self):
         return self.test
 
-    def grow_graph(self):
+    def seek_pathways(self):
         return
 
-    def combine_graphs(self,tree1,tree2):
+    def seek_products(self):
         return
 
-    def query_graph_details(self):
+    def seek_substrates(self):
+        return
+
+    def __grow_graph(self):
+        return
+
+    def __combine_graphs(self,tree1,tree2):
+        return
+
+    def __query_graph_details(self):
         return
